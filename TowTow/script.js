d50c5de300a9347a7dbf068beb338f549d688cb0
Grzywa1234 <47593899+Grzywa1234@users.noreply.github.com>
2020-05-27 11:35:12 +0200

    mobile menu fixed

--- TowTow/script.js
+++ TowTow/script.js
@@ -1,6 +1,11 @@
 const showMenuBtn = document.querySelector('.fa-bars');
 const hideMenuBtn  = document.querySelector('.fa-times-circle');
-const menuBtn = document.querySelector('.menuBtn');
+const menuBtn = document.querySelector('.menuBtn1');
+const menuBtn2 = document.querySelector('.menuBtn2');
+const menuBtn3 = document.querySelector('.menuBtn3');
+const menuBtn4 = document.querySelector('.menuBtn4');
+const menuBtn5 = document.querySelector('.menuBtn5');
+const menuBtn6 = document.querySelector('.menuBtn6');
 const menu = document.querySelector('.menu');
 
 function show() {
@@ -15,4 +20,9 @@ function hide() {
 
 
 menuBtn.addEventListener('click', hide);
+menuBtn2.addEventListener('click', hide);
+menuBtn3.addEventListener('click', hide);
+menuBtn4.addEventListener('click', hide);
+menuBtn5.addEventListener('click', hide);
+menuBtn6.addEventListener('click', hide);
 hideMenuBtn.addEventListener('click', hide);
