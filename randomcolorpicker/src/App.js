2045e64d60cd6b6f8e0318df2e7b58eb34b720e3
Grzywa1234 <47593899+Grzywa1234@users.noreply.github.com>
2020-06-03 13:12:32 +0200

    Add files via upload

---
+++ randomcolorpicker/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Random from './components/random'
+
+function App() {
+  return (
+    <div className="App">
+      <Random />
+  </div>
+  );
+}
+
+export default App;
